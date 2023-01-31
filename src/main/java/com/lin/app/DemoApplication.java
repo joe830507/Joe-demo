ada32b180709fdbe27a999dc75b17f48310ccb10
Joe <joe830507@gmail.com>
2023-01-31 21:30:19 +0800

    Update DemoApplication.java
    includes all packages with the prefix 'com.lin'

--- src/main/java/com/lin/app/DemoApplication.java
+++ src/main/java/com/lin/app/DemoApplication.java
@@ -3,7 +3,7 @@ package com.lin.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.lin")
 public class DemoApplication {
 
 	public static void main(String[] args) {
