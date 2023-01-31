6ad1b55cfbe5c4382628a564f90ebf2d29ecc40b
Joe <joe830507@gmail.com>
2023-01-31 23:20:32 +0800

    Create CommonConstant.java
    JSON_UTF8 string created.

---
+++ src/main/java/com/lin/constant/CommonConstant.java
@@ -0,0 +1,8 @@
+package com.lin.constant;
+
+import org.springframework.http.MediaType;
+
+public class CommonConstant {
+
+	public static final String JSON_UTF8 = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8";
+}
